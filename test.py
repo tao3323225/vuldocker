0dab03ea0cba57072406007314b30c878a1f8a69
tao3323225 <371200527@qq.com>
2018-08-13 17:55:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 m.changyoyo.com
 centchain.com
+public static void main(String[] args) {
+		// Mail mail = new Mail();
+		// mail.setSendEmail("test@centchain.com");
+		// ArrayList<String> rEmail = new ArrayList<>();
+		// rEmail.add("test@centchain.com");
